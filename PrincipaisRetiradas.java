5919ad221a43eedf75aee557e18517448bdf9266
Guza xt <gustavomom1227@gmail.com>
2023-03-07 14:47:21 +0000

    salvo

---
+++ PrincipaisRetiradas.java
@@ -0,0 +1,12 @@
+public class PrincipaisRetiradas {
+    public static void main(String[] args) {
+        int primeiraSemana = 4;
+        int segundaSemana = 5;
+        int terceiraSemana = 2;
+        int quartaSemana = 7;
+
+        int totalMes = primeiraSemana + segundaSemana + terceiraSemana + quartaSemana;
+
+        System.out.println("O total do mês é "+totalMes);
+    }
+}
